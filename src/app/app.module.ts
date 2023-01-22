59b0bbd0e359f256850157e29a9c7c55d37d6bc1
Gaurav N V <gauravnv1628@gmail.com>
2023-01-22 20:30:28 +0530

    Initial Ui for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RoutingComponents
   ],
   imports: [
     BrowserModule,
